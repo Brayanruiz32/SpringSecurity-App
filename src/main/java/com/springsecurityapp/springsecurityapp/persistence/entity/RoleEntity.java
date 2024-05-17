9f54e5b1ced24dff720b39743d2cb89f48a93559
Brayanruiz32 <maverick.brm@gmail.com>
2024-05-16 23:58:47 -0500

    actualizacion de mapeo de entidades

--- src/main/java/com/springsecurityapp/springsecurityapp/persistence/entity/RoleEntity.java
+++ src/main/java/com/springsecurityapp/springsecurityapp/persistence/entity/RoleEntity.java
@@ -1,12 +1,21 @@
 package com.springsecurityapp.springsecurityapp.persistence.entity;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,7 +30,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Entity
 @Table(name = "roles")
-public class RolesEntity {
+public class RoleEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; 
@@ -29,5 +38,11 @@ public class RolesEntity {
     @Column(name = "role_name")
     @Enumerated(EnumType.STRING)
     private RoleEnum roleEnum;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "role_permissions", joinColumns = @JoinColumn(name = "role_id"), inverseJoinColumns = @JoinColumn(name = "permission_id"))
+    private Set<PermissionEntity> permissionList = new HashSet<>();  
+    // private List<UserEntity> users;
+
 
 }
--- src/main/java/com/springsecurityapp/springsecurityapp/persistence/entity/UserEntity.java
+++ src/main/java/com/springsecurityapp/springsecurityapp/persistence/entity/UserEntity.java
@@ -1,10 +1,21 @@
 package com.springsecurityapp.springsecurityapp.persistence.entity;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -43,8 +54,9 @@ public class UserEntity {
     @Column(name = "credential_no_expired")
     private boolean credentialNoExpired;
 
-
-
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "rol_id"))
+    private Set<RoleEntity> roles = new HashSet<>();
 
 
 
